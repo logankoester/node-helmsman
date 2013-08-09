def7267fcfddd040b84438442b495df3fbe9d699
Beau Gunderson <beau@beaugunderson.com>
2013-08-09 10:51:04 -0700

    Add support for extensions and commands in PATH

--- helmsman.js
+++ helmsman.js
@@ -3,21 +3,24 @@
 /*!
  * Module dependencies.
  */
-var fs = require('fs');
 var util = require('util');
 var events = require("events");
 var path = require('path');
 var glob = require('glob');
-var colors = require('colors');
 var spawn = require('child_process').spawn;
 var _s = require('underscore.string');
 var domain = require('domain').create();
+var _ = require('lodash');
+
+require('colors');
+
 /**
  * Exports
  */
-module.exports = exports = helmsman;
-exports.Helmsman = Helmsman;
 
+module.exports = exports = helmsman;
+
+exports.Helmsman = Helmsman;
 
 /**
  * The Helmsman constructor
@@ -28,12 +31,16 @@ exports.Helmsman = Helmsman;
  *
  * @param  {object} options   config
  */
-function Helmsman(options){
+function Helmsman(options) {
   var self = this;
 
   events.EventEmitter.call(self);
 
   if (!options) { options = {}; }
+
+  if (options.usePath) {
+    this.usePath = true;
+  }
 
   if (!options.localDir) {
     this.localDir = path.dirname(module.parent.filename);
@@ -41,12 +48,12 @@ function Helmsman(options){
     this.localDir = path.resolve(options.localDir);
   }
 
-  // Guess the prefix. Assume if one isn't given and that executable doesn't equal 
+  // Guess the prefix. Assume if one isn't given and that executable doesn't equal
   // the root command filename, use the filename of the root command
   if (!options.prefix && path.basename(process.argv[1]) !== path.basename(require.main.filename)) {
-    this.prefix = path.basename(require.main.filename)
+    this.prefix = path.basename(require.main.filename, path.extname(require.main.filename));
   } else {
-    this.prefix = options.prefix || path.basename(process.argv[1]);
+    this.prefix = options.prefix || path.basename(process.argv[1], path.extname(process.argv[1]));
   }
 
   this.availableCommands = {};
@@ -58,28 +65,59 @@ function Helmsman(options){
   }
 
   // Local files in files in the /bin folder for an application
-  this.localFiles = glob.sync(self.prefix+"*", {cwd: self.localDir});
-  
-  this.localFiles.forEach(function(file){
-    // Figure out the longest command name for printing --help
-    var commandData = require(path.join(self.localDir, file)).command;
+  this.localFiles = glob.sync(self.prefix + "*", { cwd: self.localDir })
+    .map(function (file) {
+      return path.join(self.localDir, file);
+    });
 
-    if (!commandData) {
-      util.error('The file ('+file+') did not export.command. Please ensure your commands are setup properly and your prefix is correct'.red);
-      process.exit(1);
+  if (this.usePath) {
+    var pathTokens = process.env.PATH.split(':');
+
+    pathTokens.forEach(function (pathToken) {
+      self.localFiles = self.localFiles.concat(glob.sync(self.prefix + '*',
+        { cwd: pathToken }).map(function (file) {
+          return path.join(pathToken, file);
+        }));
+    });
+  }
+
+  this.localFiles.forEach(function(file) {
+    var extension = path.extname(file);
+    var name = path.basename(file, extension).substr(self.prefix.length);
+
+    var commandData = {
+      description: ''
+    };
+
+    // Only try to get metadata from commands written in JavaScript
+    if (extension === '' || extension === '.js') {
+      try {
+        commandData = _.merge(commandData, require(file).command);
+      } catch (e) {
+        // If it is JavaScript and we fail to require it's an error
+        if (extension === '.js') {
+          util.error(util.format('The file "%s" did not load correctly: %s', file, e).red);
+          process.exit(1);
+        }
+      }
     }
 
-    self.availableCommands[file.substr(self.prefix.length)] = commandData;
+    commandData.path = file;
 
-    var fullCommand = (commandData.options) ? file + ' ' + commandData.command : file
+    self.availableCommands[name] = commandData;
 
-    if (fullCommand.length > self.commandMaxLength) { self.commandMaxLength = file.length; }
+    var fullCommand = (commandData.options) ? name + ' ' + commandData.command : name;
+
+    // Figure out the longest command name for printing --help
+    if (fullCommand.length > self.commandMaxLength) {
+      self.commandMaxLength = name.length;
+    }
   });
 
   self.availableCommands['help'] = { // help is always available!
     arguments: '<sub-command>',
     description: 'Show the --help for a specific command'
-  }
+  };
 }
 
 util.inherits(Helmsman, events.EventEmitter);
@@ -94,12 +132,12 @@ util.inherits(Helmsman, events.EventEmitter);
  *
  *   var cli = helmsman();
  *   cli.parse();
- * 
+ *
  * @param  {Object} options   Contructor options
  * @return {Helmsman}         A new helmsman
  */
 
-function helmsman(options){
+function helmsman(options) {
   return new Helmsman(options);
 }
 
@@ -111,26 +149,26 @@ function helmsman(options){
  *    * Explicit match (status === status)
  *    * Shorthand (st === status)
  *    * Levenshtein of <=2 (sratus === status)
- * 
+ *
  * @param  {String} cmd The command given to the script
  * @param  {[String]} availableCommands An array of all the available commands
  * @return {String}     The actual command that will be run
  */
-Helmsman.prototype.getCommand = function(cmd, availableCommands){
+Helmsman.prototype.getCommand = function(cmd, availableCommands) {
   var self = this;
-  
+
   if (!availableCommands) {
     availableCommands = Object.keys(self.availableCommands);
   }
-  
+
   // If there is an exact match, return it
   if (~availableCommands.indexOf(cmd)) {
     return cmd;
   }
 
-  // Detirmine how many commands match the iterator. Return one if command, 
-  function isOneOrMore(commands, iterator){
-    var list = commands.filter(iterator)
+  // Determine how many commands match the iterator. Return one if command,
+  function isOneOrMore(commands, iterator) {
+    var list = commands.filter(iterator);
 
     if (list.length === 1) {
       return list[0];
@@ -142,20 +180,20 @@ Helmsman.prototype.getCommand = function(cmd, availableCommands){
   }
 
   // If there is a shorthand match, return it
-  var shortHandCmd = isOneOrMore(availableCommands, function(command){ 
+  var shortHandCmd = isOneOrMore(availableCommands, function(command) {
     return (command.indexOf(cmd) === 0);
-  })
+  });
   if (shortHandCmd) { return shortHandCmd; }
 
   // If there is a close match, return it
-  var similarCmd = isOneOrMore(availableCommands, function(command){ 
+  var similarCmd = isOneOrMore(availableCommands, function(command) {
     return (_s.levenshtein(cmd, command) <= 2);
-  })
+  });
   if (similarCmd) { return similarCmd; }
 
   // If nothing, then get outta here
   return new Error(util.format('There are no commands by the name of "%s"', cmd));
-}
+};
 
 /**
  * GO!
@@ -163,7 +201,7 @@ Helmsman.prototype.getCommand = function(cmd, availableCommands){
  * @param {[Object]} argv   The process arguments to parse. Defaults to process.argv
  */
 
-Helmsman.prototype.parse = function(argv){
+Helmsman.prototype.parse = function(argv) {
   var self = this;
 
   argv = argv || process.argv; // If no arguments are passed, assume process.argv
@@ -172,22 +210,24 @@ Helmsman.prototype.parse = function(argv){
 
   // Much of the following heavily inspired or simply taken from component/bin
   // https://github.com/component/component/blob/master/bin/component
-  
+
   // Print the modules version number
   if (args[0] === '--version') {
     // BOLD assumption that the file is in ./bin
     var packagePath = path.join(path.dirname(require.main.filename), '..', 'package.json');
     var pkg = require(packagePath);
-    return console.log(pkg.name + ": " + pkg.version)
+    return console.log(pkg.name + ": " + pkg.version);
   }
 
   // Print the command list if --help is called
-  if (args[0] === '--help' || !args.length || args[0][0] === '-' || (args[0] === 'help' && args.length === 1)) {
+  if (args[0] === '--help' ||
+    !args.length ||
+    args[0][0] === '-' ||
+    (args[0] === 'help' && args.length === 1) ||
+    (self.getCommand(args[0]) === 'help' && args.length === 1)) {
     self.emit('--help');
     return self.showHelp();
   }
-
-  var cmd = self.getCommand(args.shift());
 
   if ('object' === typeof cmd && cmd.name === "Error") {
     util.error(cmd.message.red);
@@ -195,30 +235,33 @@ Helmsman.prototype.parse = function(argv){
     process.exit(1);
   }
 
+  var cmd = self.getCommand(args.shift());
+
   // Implicit help
   // If <command> help <sub-command> is entered, automatically run <command>-<sub-command> --help
   if (cmd === 'help') {
     cmd = args.shift();
     args = ['--help'];
   }
-  
-  var binPath = path.join(self.localDir, self.prefix + cmd);
+
+  var fullPath = self.availableCommands[cmd] &&
+    self.availableCommands[cmd].path;
 
   domain.on('error', function(err) {
     if (err.code === 'EACCES') {
       console.error('');
       console.error('Could not execute the subcommand: ' + self.prefix + cmd);
       console.error('');
-      console.error('Consider running:\n chmod +x '+binPath);
+      console.error('Consider running:\n chmod +x', fullPath);
     } else {
       console.error(err.stack.red);
     }
   });
-  
-  domain.run(function() {
-    var subcommand = spawn(binPath, args, { stdio: 'inherit' });
 
-    subcommand.on('close', function(code){
+  domain.run(function() {
+    var subcommand = spawn(fullPath, args, { stdio: 'inherit' });
+
+    subcommand.on('close', function(code) {
       process.exit(code);
     });
   });
@@ -229,17 +272,17 @@ Helmsman.prototype.parse = function(argv){
  * Show the help
  */
 
-Helmsman.prototype.showHelp = function(){
+Helmsman.prototype.showHelp = function() {
   var self = this;
   var fullCommands = [];
 
   console.log('');
   console.log('Commands:');
   console.log('');
-  
+
   for (var command in self.availableCommands) {
     var fullCommand = command;
-    // console.log(command, self.availableCommands[command].options, "\n")
+
     if (self.availableCommands[command].arguments) {
       fullCommand += ' ' + self.availableCommands[command].arguments;
     }
@@ -251,14 +294,16 @@ Helmsman.prototype.showHelp = function(){
     fullCommands.push([fullCommand, self.availableCommands[command].description]);
   }
 
-  fullCommands.forEach(function(command){
+  fullCommands.forEach(function(command) {
     var diff = (self.commandMaxLength - command[0].length);
+
     // Pad spaces at the end of each command so help descriptions line up
     for (var i = 0; i < diff; i++) {
-      command[0]+=' ';
+      command[0] += ' ';
     }
+
     console.log('   %s     %s', command[0], command[1]);
-  })
+  });
 
   process.exit();
 };
--- test/local.test.js
+++ test/local.test.js
@@ -5,11 +5,13 @@ var helmsman = require('..');
 var cli = helmsman({ prefix: 'testcommand', localDir: './bin'});
 
 test('construct an instance of a helmsman', function(t){
-  t.plan(3);
+  t.plan(5);
 
   t.equal(cli.localDir.substr(-8), 'test/bin', 'The localDir is set');
   t.equal(cli.prefix, 'testcommand-', 'The prefix is properly set');
   t.equal(cli.availableCommands.subcommand.description, 'A test', 'A subcommand\'s meta data is loaded');
+  t.equal(cli.availableCommands.subcommandWithExtension.description, 'A test', 'A subcommand\'s meta data is loaded');
+  t.equal(cli.availableCommands.subcommandInPython.description, '', 'A non-JavaScript command doesn\'t have a description');
 });
 
 test('Guess the right command', function(t){
@@ -20,6 +22,6 @@ test('Guess the right command', function(t){
   t.equal(cli.getCommand('status', availableCommands), 'status', '"status" returned status');
   t.equal(cli.getCommand('st', availableCommands), 'status', '"st" returned status');
   t.equal(cli.getCommand('isntall', availableCommands), 'install', '"isntall" returned install');
-  t.similar(cli.getCommand('in', availableCommands), {message:'There are 2 potential options for "in": install,info'})
-  t.similar(cli.getCommand('delete', availableCommands), {message:'There are no commands by the name of "delete"'})
-})
+  t.similar(cli.getCommand('in', availableCommands), {message:'There are 2 potential options for "in": install,info'});
+  t.similar(cli.getCommand('delete', availableCommands), {message:'There are no commands by the name of "delete"'});
+});
